7aae4a3b9e210b040c6f8143dd3eb4959aaaa8ba
Aleksandr Kelbas <alex@motionly.co.uk>
2016-05-14 22:51:30 +0100

    Refactored CityList init and load tests and now passing

--- WeatherAlert/CityListHandler.swift
+++ WeatherAlert/CityListHandler.swift
@@ -7,25 +7,42 @@
 //
 
 import Foundation
-
+import ObjectMapper
 
 class CityList {
     
-    private var cities: [City] = []
+    private(set) var cities: [City]?
+    private(set) var fileName: String
+    private var jsonString: String?
     
-    init(fileName: String) {
+    
+    init?(fileName: String, newLineDelimited: Bool = false) {
+        self.fileName = fileName
         let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: "json")
         do {
             let data = try NSData(contentsOfFile: filePath!, options: .DataReadingMappedIfSafe)
-            let json = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers)
-            
+            jsonString = String(data: data, encoding: NSUTF8StringEncoding)
+            if newLineDelimited == true && jsonString != nil {
+                // As NSJSONSerialisation doesn't support newline delimited JSON - add commas and
+                jsonString = String(format: "[%@]", jsonString!.stringByReplacingOccurrencesOfString("\n", withString: ","))
+            }
         } catch let error as NSError {
             print("Could not load json file named: \(fileName). Error: \(error)")
+            return nil
         }
     }
     
-    func getAll(completion: (cities: [City], error: NSError?)->()) {
-        completion(cities: cities, error: nil)
+    func load(completion: ((cities: [City]?)->())?) {
+        guard cities == nil else {
+            completion?(cities: cities)
+            return
+        }
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { [weak self] in
+            self?.cities = Mapper<City>().mapArray(self?.jsonString)
+            dispatch_async(dispatch_get_main_queue(), { [weak self] in
+                completion?(cities: self?.cities)
+            })
+        })
     }
-    
 }
--- WeatherAlertTests/CityListHandlerTests.swift
+++ WeatherAlertTests/CityListHandlerTests.swift
@@ -22,26 +22,40 @@ class CityListHandlerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testCityListLoading() {
+    func testCityListInitializer() {
         // intialise cityList from file
-        let cityList = CityList(fileName: "")
+        let cityList = CityList(fileName: "city.list", newLineDelimited: true)
+
         // city list will be nil if it failed to load file
         XCTAssertNotNil(cityList, "City list must not be nil");
-        // We'll be getting cities in the background thread, so we'll need to wait for that to finish.
-        let expectation = expectationWithDescription("Did load cities list")
         
-        // Get all cities, just test that we can get cities
-        cityList.getAll { (cities: [City], error: NSError?) in
-            // There shouldn't be any errors
-            XCTAssertNil(error, "There was an error getting cities from json file : \(error)")
-            // There must some cities in the file
-            XCTAssertGreaterThan(cities.count, 0, "There must be at least 1 city in the file")
-            // Fullfill expectation
+        let cities = cityList?.cities
+        // at this point cities shouldn't be loaded
+        XCTAssertNil(cities, "Cities should only be initialized when requested")
+    }
+    
+    func testCityListLoading() {
+        let cityList = CityList(fileName: "city.list", newLineDelimited: true)
+        XCTAssertNotNil(cityList)
+        
+        let expectation = expectationWithDescription("City list loaded into memory")
+        // citylist loading must happen in background as it's quite a big file.
+        cityList?.load { cities in
+            
+            XCTAssertNotNil(cities, "Cities object must not be nil, it seems json loading failed")
+            XCTAssertGreaterThan(cities!.count, 0, "There must be some cities in the list")
+            
+            let city = cities?.first
+            XCTAssertEqual(city?.id, 707860)
+            XCTAssertEqual(city?.name, "Hurzuf")
+            XCTAssertEqual(city?.country, "UA")
+            
+            let coord = city?.coord
+            XCTAssertEqual(coord?.lon, 34.283333)
+            XCTAssertEqual(coord?.lat, 44.549999)
             expectation.fulfill()
         }
-        
-        // We'll allow 5 seconds to wait, as this read should be quite quick.
-        waitForExpectationsWithTimeout(5, handler: nil)
+        waitForExpectationsWithTimeout(30, handler: nil)
     }
     
     func testCityObjectMapping() {
