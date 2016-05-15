80e6b58c4d66aef242343683d562619c12038ed0
Aleksandr Kelbas <alex@motionly.co.uk>
2016-05-15 20:46:55 +0100

    Basic request handler test passing

--- WeatherAlert/API.swift
+++ WeatherAlert/API.swift
@@ -16,8 +16,50 @@ class API {
         return NSURL(string: "http://api.openweathermap.org/data/2.5")!
     }
     
+    typealias APIResponseBlock = ((response: AnyObject?, error: NSError?) -> ())
+
+    /**
+     Initialises API instance with given appID
+     
+     - parameter appID: App ID as provided by Open Wather
+     
+     - returns: Instance of API class
+     */
     init(appID: String) {
         self.appID = appID
     }
     
+    /**
+     Creates an NSURLSession with default configuration and application/json content-type header
+     
+     - returns: NSURLSession should be used for OpenWather API
+     */
+    func getSession() -> NSURLSession {
+        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        sessionConfig.HTTPAdditionalHeaders = ["Content-Type" : "application/json"]
+        let session = NSURLSession(configuration: sessionConfig)
+        return session
+    }
+    
+    
+    // MARK: - Main handlers
+    func handleURLRequest(request: NSURLRequest, completion: APIResponseBlock) {
+        let dataTask = getSession().dataTaskWithRequest(request, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) in
+
+            guard error == nil else  {
+                completion(response: response, error: error)
+                return
+            }
+            
+            do {
+                let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+                completion(response: jsonData, error: nil)
+            } catch let err as NSError {
+                completion(response: response, error: err)
+            }
+            
+        })
+        
+        dataTask.resume()
+    }
 }
--- WeatherAlertTests/APITests.swift
+++ WeatherAlertTests/APITests.swift
@@ -40,4 +40,49 @@ class APITests: XCTestCase {
         XCTAssertEqual(api_shared.serverURL, NSURL(string: "http://api.openweathermap.org/data/2.5"))
     }
     
+    func testURLSessionConfiguration() {
+        let session = API.sharedInstance.getSession()
+        let contentType = session.configuration.HTTPAdditionalHeaders?["Content-Type"]
+        XCTAssertNotNil(contentType)
+        XCTAssertEqual(contentType as? String, "application/json")
+    }
+    
+    
+    func testURLRequestHandler() {
+        // for this test we'll be using different API
+        let api = API(appID: "any")
+        let testURL = NSURL(string: "http://headers.jsontest.com")!
+        let request = NSURLRequest(URL: testURL)
+        
+        let testExpectation = expectationWithDescription("API can successfuly make request to pages with json response")
+        
+        api.handleURLRequest(request) { (response: AnyObject?, error: NSError?) in
+            XCTAssertNil(error)
+            XCTAssertNotNil(response)
+            // respopnse must be a json decoded object for this request
+            XCTAssertTrue(response is [String : AnyObject])
+            testExpectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
+    
+    
+    func testURLRequestHandlerFailure() {
+        let api = API(appID: "any")
+        let testURL = NSURL(string: "http://google.com")!
+        let request = NSURLRequest(URL: testURL)
+        
+        let testExpectation = expectationWithDescription("API can handle failures gracefully")
+        
+        api.handleURLRequest(request) { (response: AnyObject?, error: NSError?) in
+            // as this url doesn't provides json response - we should see an error here
+            XCTAssertNotNil(error)
+            testExpectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
+    
+    
 }
